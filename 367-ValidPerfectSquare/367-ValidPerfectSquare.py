c3cb887bc691a8a19261a26a13465d92ce080faf
faheemcr034 <faheemcr034@gmail.com>
2026-07-09 10:08:22 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.py
@@ -0,0 +1,15 @@
+# Last updated: 09/07/2026, 10:08:21
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l = 0
+        r = num
+        while l<=r:
+            mid = (l+r) // 2
+            if mid*mid == num:
+                return True
+            elif mid*mid < num:
+                l = mid + 1
+            else:
+                r = mid - 1    
+        return False
+        
